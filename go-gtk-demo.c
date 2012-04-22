319ce8d77fb316032456ff743b22dee77831edea
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2012-04-22 22:44:15 +0200

    pass command line arguments to gtk

--- go-gtk-demo.c
+++ go-gtk-demo.c
@@ -2,12 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+char **return_argv;
+
+char *return_arg(int i)
+{
+    return return_argv[i];
+}
+
+int init(int argc, char **argv)
+{
+    int ac, i;
+
+    ac = argc;
+    return_argv = (char **) malloc (argc * sizeof(char *));
+    for (i = 0; i < argc; i++)
+	return_argv[i] = strdup(argv[i]);
+
+    /* This is called in all GTK applications. Arguments are parsed
+     * from the command line and are returned to the application. */
+    gtk_init (&ac, &return_argv);
+
+    return ac;
+}
+
 static void hello( GtkWidget *widget,
                    gpointer   data )
 {
-    char *s = strdup("Hello World");
-    go_message(1, s);
-    free(s);
+    go_message(1, "Hello World");
 }
 
 static gboolean delete_event( GtkWidget *widget,
@@ -20,8 +41,7 @@ static gboolean delete_event( GtkWidget *widget,
      * This is useful for popping up 'are you sure you want to quit?'
      * type dialogs. */
 
-    char *s = "Delete!";
-    go_message(0, s);
+    go_message(1, "Delete!");
 
     return FALSE;
 }
@@ -30,27 +50,19 @@ static void destroy( GtkWidget *widget,
                      gpointer   data )
 {
     gtk_main_quit ();
+
+    go_message(0, "Destroy!");
 }
 
 void setup() {
-    int argc;
-    char **argv;
 
     /* GtkWidget is the storage type for widgets */
     GtkWidget *window;
     GtkWidget *button;
-    
-    /* This is called in all GTK applications. Arguments are parsed
-     * from the command line and are returned to the application. */
-    /* TO DO: pass command line argument from/to Go */
-    argc = 1;
-    argv = (char **) malloc(sizeof (char *));
-    argv[0] = strdup("Go GTK+ Demo");
-    gtk_init (&argc, &argv);
-    
+
     /* create a new window */
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-    
+
     /* When the window is given the "delete-event" signal (this is given
      * by the window manager, usually by the "close" option, or on the
      * titlebar), we ask it to call the delete_event () function
@@ -58,25 +70,25 @@ void setup() {
      * function is NULL and is ignored in the callback function. */
     g_signal_connect (window, "delete-event",
 		      G_CALLBACK (delete_event), NULL);
-    
-    /* Here we connect the "destroy" event to a signal handler.  
+
+    /* Here we connect the "destroy" event to a signal handler.
      * This event occurs when we call gtk_widget_destroy() on the window,
      * or if we return FALSE in the "delete-event" callback. */
     g_signal_connect (window, "destroy",
 		      G_CALLBACK (destroy), NULL);
-    
+
     /* Sets the border width of the window. */
     gtk_container_set_border_width (GTK_CONTAINER (window), 100);
-    
+
     /* Creates a new button with the label "Hello World". */
     button = gtk_button_new_with_label ("Hello World");
-    
+
     /* When the button receives the "clicked" signal, it will call the
      * function hello() passing it NULL as its argument.  The hello()
      * function is defined above. */
     g_signal_connect (button, "clicked",
 		      G_CALLBACK (hello), NULL);
-    
+
     /* This will cause the window to be destroyed by calling
      * gtk_widget_destroy(window) when "clicked".  Again, the destroy
      * signal could come from here, or the window manager. */
@@ -89,13 +101,13 @@ void setup() {
 
     /* This packs the button into the window (a gtk container). */
     gtk_container_add (GTK_CONTAINER (window), button);
-    
+
     /* The final step is to display this newly created widget. */
     gtk_widget_show (button);
-    
+
     /* and the window */
     gtk_widget_show (window);
-    
+
     /* All GTK applications must have a gtk_main(). Control ends here
      * and waits for an event to occur (like a key press or
      * mouse event). */
--- go-gtk-demo.go
+++ go-gtk-demo.go
@@ -3,12 +3,18 @@ package main
 /*
 #cgo pkg-config: gtk+-2.0
 #include <gtk/gtk.h>
+#include <stdlib.h>
+char *return_arg(int i);
+int init(int argc, char **argv);
 void setup(void);
 */
 import "C"
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"unsafe"
 )
 
 var (
@@ -26,8 +32,39 @@ func go_message(id int, cContent *C.char) {
 	msgCh <- msg{id: id, ms: content}
 }
 
+func initGTK() {
+
+	// pass command line arguments to GTK+
+	argc := len(os.Args)
+	argv := make([]*C.char, 0, argc)
+	for _, arg := range os.Args {
+		argv = append(argv, C.CString(arg))
+	}
+	argc = int(C.init(C.int(argc), &argv[0]))
+
+	// clean-up
+	for _, a := range argv {
+		C.free(unsafe.Pointer(a))
+	}
+
+	// update os.Args
+	os.Args = os.Args[0:0]
+	for i := 0; i < argc; i++ {
+		os.Args = append(os.Args, C.GoString(C.return_arg(C.int(i))))
+	}
+
+	fmt.Println("Remaining command line arguments:", os.Args)
+}
+
 func main() {
+
+	initGTK()
+
+	flag.Parse()
+	fmt.Println("Remaining flag.Args():", flag.Args())
+
 	go C.setup()
+
 	for {
 		m := <-msgCh
 		fmt.Println(m.ms)
