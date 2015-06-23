98dd5d7aa43cf16f05359c030d6e576ad6ff61aa
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2015-06-23 22:21:11 +0200

    using builder

--- go-gtk-demo.c
+++ go-gtk-demo.c
@@ -3,6 +3,22 @@
 #include <string.h>
 #include "_cgo_export.h"
 
+static const gchar *interface =
+    "<interface>"
+    "  <object class=\"GtkWindow\" id=\"main-window\">"
+    "    <property name=\"title\">Go/GTK+ Demo</property>"
+    "    <property name=\"border-width\">100</property>"
+    "    <signal name=\"destroy\" handler=\"destroy\"/>"
+    "    <signal name=\"delete-event\" handler=\"delete_event\"/>"
+    "    <child>"
+    "      <object class=\"GtkButton\" id=\"my-button\">"
+    "        <property name=\"label\">Hello World</property>"
+    "        <signal name=\"clicked\" handler=\"hello\"/>"
+    "      </object>"
+    "    </child>"
+    "  </object>"
+    "</interface>";
+
 char **return_argv;
 
 char *return_arg(int i)
@@ -29,13 +45,13 @@ int init(int argc, void *argv)
     return ac;
 }
 
-static void hello( GtkWidget *widget,
+G_MODULE_EXPORT void hello( GtkWidget *widget,
                    gpointer   data )
 {
     go_message(1, "Hello World");
 }
 
-static gboolean delete_event( GtkWidget *widget,
+G_MODULE_EXPORT gboolean delete_event( GtkWidget *widget,
                               GdkEvent  *event,
                               gpointer   data )
 {
@@ -50,7 +66,7 @@ static gboolean delete_event( GtkWidget *widget,
     return FALSE;
 }
 
-static void destroy( GtkWidget *widget,
+G_MODULE_EXPORT void destroy( GtkWidget *widget,
                      gpointer   data )
 {
     gtk_main_quit ();
@@ -60,60 +76,14 @@ static void destroy( GtkWidget *widget,
 
 void setup() {
 
-    /* GtkWidget is the storage type for widgets */
+    GtkBuilder *builder;
+    GError *error = NULL;
     GtkWidget *window;
-    GtkWidget *button;
 
-    /* create a new window */
-    window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-
-    /* When the window is given the "delete-event" signal (this is given
-     * by the window manager, usually by the "close" option, or on the
-     * titlebar), we ask it to call the delete_event () function
-     * as defined above. The data passed to the callback
-     * function is NULL and is ignored in the callback function. */
-    g_signal_connect (window, "delete-event",
-		      G_CALLBACK (delete_event), NULL);
-
-    /* Here we connect the "destroy" event to a signal handler.
-     * This event occurs when we call gtk_widget_destroy() on the window,
-     * or if we return FALSE in the "delete-event" callback. */
-    g_signal_connect (window, "destroy",
-		      G_CALLBACK (destroy), NULL);
-
-    /* Sets the border width of the window. */
-    gtk_container_set_border_width (GTK_CONTAINER (window), 100);
-
-    /* Creates a new button with the label "Hello World". */
-    button = gtk_button_new_with_label ("Hello World");
-
-    /* When the button receives the "clicked" signal, it will call the
-     * function hello() passing it NULL as its argument.  The hello()
-     * function is defined above. */
-    g_signal_connect (button, "clicked",
-		      G_CALLBACK (hello), NULL);
-
-    /* This will cause the window to be destroyed by calling
-     * gtk_widget_destroy(window) when "clicked".  Again, the destroy
-     * signal could come from here, or the window manager. */
-
-    /*
-    g_signal_connect_swapped (button, "clicked",
-			      G_CALLBACK (gtk_widget_destroy),
-                              window);
-    */
-
-    /* This packs the button into the window (a gtk container). */
-    gtk_container_add (GTK_CONTAINER (window), button);
-
-    /* The final step is to display this newly created widget. */
-    gtk_widget_show (button);
-
-    /* and the window */
-    gtk_widget_show (window);
-
-    /* All GTK applications must have a gtk_main(). Control ends here
-     * and waits for an event to occur (like a key press or
-     * mouse event). */
+    builder = gtk_builder_new ();
+    gtk_builder_add_from_string (builder, interface, -1, &error);
+    gtk_builder_connect_signals (builder, NULL);
+    window = GTK_WIDGET(gtk_builder_get_object (builder, "main-window"));
+    gtk_widget_show_all (window);
     gtk_main ();
 }
